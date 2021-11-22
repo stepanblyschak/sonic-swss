70d2c7da0b5ed0da9b11f8a5b0939a0ea8fcdbe9
Stepan Blyshchak <stepanb@nvidia.com>
2021-11-22 19:15:40 +0200

    fix conflicts
    Signed-off-by: Stepan Blyshchak <stepanb@nvidia.com>

--- orchagent/aclorch.cpp
+++ orchagent/aclorch.cpp
@@ -1164,7 +1164,7 @@ bool AclRule::setAction(sai_acl_entry_attr_t actionId, sai_acl_action_data_t act
 
     m_actions[actionId] = SaiAttrWrapper(SAI_OBJECT_TYPE_ACL_ENTRY, attr);
 
-    if (!m_pTable->validateAclRuleAction(attrId, *this))
+    if (!m_pTable->validateAclRuleAction(actionId, *this))
     {
         return false;
     }
