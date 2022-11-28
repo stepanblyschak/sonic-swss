8926ddd5553ed1d85572cfc7293df81b9a3c1a4d
Stepan Blyschak <stepanb@nvidia.com>
2022-11-28 17:03:24 +0000

    style
    Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>

--- tests/mock_tests/fake_warmstarthelper.cpp
+++ tests/mock_tests/fake_warmstarthelper.cpp
@@ -4,38 +4,76 @@ static swss::DBConnector gDb("APPL_DB", 0);
 
 namespace swss {
 
-WarmStartHelper::WarmStartHelper(RedisPipeline      *pipeline,
-                ProducerStateTable *syncTable,
-                const std::string  &syncTableName,
-                const std::string  &dockerName,
-                const std::string  &appName) : m_restorationTable(&gDb, "") {}
+WarmStartHelper::WarmStartHelper(RedisPipeline *pipeline,
+                                 ProducerStateTable *syncTable,
+                                 const std::string &syncTableName,
+                                 const std::string &dockerName,
+                                 const std::string &appName) :
+    m_restorationTable(&gDb, "")
+{
+}
 
-WarmStartHelper::~WarmStartHelper() {}
+WarmStartHelper::~WarmStartHelper()
+{
+}
 
-void WarmStartHelper::setState(WarmStart::WarmStartState state) {}
+void WarmStartHelper::setState(WarmStart::WarmStartState state)
+{
+}
 
-WarmStart::WarmStartState WarmStartHelper::getState(void) const { return WarmStart::WarmStartState::INITIALIZED; }
+WarmStart::WarmStartState WarmStartHelper::getState() const
+{
+    return WarmStart::WarmStartState::INITIALIZED;
+}
 
-bool WarmStartHelper::checkAndStart(void) { return false; }
+bool WarmStartHelper::checkAndStart()
+{
+    return false;
+}
 
-bool WarmStartHelper::isReconciled(void) const { return false; }
+bool WarmStartHelper::isReconciled() const
+{
+    return false;
+}
 
-bool WarmStartHelper::inProgress(void) const { return false; }
+bool WarmStartHelper::inProgress() const
+{
+    return false;
+}
 
-uint32_t WarmStartHelper::getRestartTimer(void) const { return 0; }
+uint32_t WarmStartHelper::getRestartTimer() const
+{
+    return 0;
+}
 
-bool WarmStartHelper::runRestoration(void) { return false; }
+bool WarmStartHelper::runRestoration()
+{
+    return false;
+}
 
-void WarmStartHelper::insertRefreshMap(const KeyOpFieldsValuesTuple &kfv) {}
+void WarmStartHelper::insertRefreshMap(const KeyOpFieldsValuesTuple &kfv)
+{
+}
 
-void WarmStartHelper::reconcile(void) {}
+void WarmStartHelper::reconcile()
+{
+}
 
-const std::string WarmStartHelper::printKFV(const std::string                  &key,
-                                            const std::vector<FieldValueTuple> &fv) { return ""; }
+const std::string WarmStartHelper::printKFV(const std::string &key,
+                                            const std::vector<FieldValueTuple> &fv)
+{
+    return "";
+}
 
 bool WarmStartHelper::compareAllFV(const std::vector<FieldValueTuple> &left,
-                                   const std::vector<FieldValueTuple> &right) { return false; }
+                                   const std::vector<FieldValueTuple> &right)
+{
+    return false;
+}
 
-bool WarmStartHelper::compareOneFV(const std::string &v1, const std::string &v2) { return false; }
+bool WarmStartHelper::compareOneFV(const std::string &v1, const std::string &v2)
+{
+    return false;
+}
 
 }
