4d7369172ef10410333fdcee75d596f21e1c0c36
Stepan Blyschak <stepanb@nvidia.com>
2024-08-21 09:11:59 +0000

    [portsorch] query SAI_HOSTIF_ATTR_QUEUE switch capability once
    Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>

--- orchagent/portsorch.h
+++ orchagent/portsorch.h
@@ -339,6 +339,7 @@ private:
     bool oper_fec_sup = false;
     bool saiHwTxSignalSupported = false;
     bool saiTxReadyNotifySupported = false;
+    bool m_supportsHostIfTxQueue = false;
 
     swss::SelectableTimer *m_port_state_poller = nullptr;
 
