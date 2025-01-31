faf75c57dc1fcad869d50414055b04a5e67efab3
dgsudharsan <sudharsand@nvidia.com>
2025-01-31 05:07:23 +0000

    [202412_RC]Bulk size per counter plus bulk counter initialization%118

--- orchagent/flexcounterorch.h
+++ orchagent/flexcounterorch.h
@@ -71,6 +71,7 @@ private:
     Table m_deviceMetadataConfigTable;
     std::unique_ptr<SelectableTimer> m_delayTimer;
     std::unique_ptr<Executor> m_delayExecutor;
+    std::unordered_set<std::string> m_groupsWithBulkChunkSize;
 };
 
 #endif
